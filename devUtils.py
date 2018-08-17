2179f590e7c055aecdafd1066bd6d6e4c592a47b
Rodrigo <drigox90rih@gmail.com>
2018-08-17 16:26:10 -0300

    update dev write

--- devUtils.py
+++ devUtils.py
@@ -1,5 +1,14 @@
 # utils
+import json
 def writeFile(data, filepathname):
-	with open(filepathname,'a') as f:
-		f.write(data)
+
+		with open(filepathname,'a') as f:
+			f.write('----- d u m p -- d a t a ---------\n')
+			if type(data) is dict:
+				f.write(json.dumps(data, sort_keys=True,indent=2,separators=(',',': ')) + '\n')
+			elif type(data) is str:
+				f.write(data + '\n')
+			else:
+				f.write(str(data) + '\n')
+
 
